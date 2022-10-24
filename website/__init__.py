c7ade6c7eb57102e88df5ed6ae93c70bba7c5f78
Gabriel Largo <largo2610@gmail.com>
2022-10-24 21:01:09 +1000

    initial commit

--- website/__init__.py
+++ website/__init__.py
@@ -39,8 +39,6 @@ def create_app():
     from . import views
     app.register_blueprint(views.bp)
     from . import auth
-    app.register_blueprint(events.bp)
-    from . import auth
     app.register_blueprint(auth.bp)
     
     return app
