f02bcf7cd5ecadc530fbe233a00e7f355f448bbc
Gabriel Largo <largo2610@gmail.com>
2022-10-24 20:46:57 +1000

    initial commit

---
+++ website/models.py
@@ -0,0 +1,50 @@
+from . import db
+from datetime import datetime
+from flask_login import UserMixin
+
+class User(db.Model, UserMixin):
+    __tablename__='users' # good practice to specify table name
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(100), index=True, unique=True, nullable=False)
+    emailid = db.Column(db.String(100), index=True, nullable=False)
+	#password is never stored in the DB, an encrypted password is stored
+	# the storage should be at least 255 chars long
+    password_hash = db.Column(db.String(255), nullable=False)
+
+    # relation to call user.comments and comment.created_by
+    comments = db.relationship('Comment', backref='user')
+
+
+
+class Events(db.Model):
+    __tablename__ = 'Events'
+    id = db.Column(db.Integer, primary_key=True)
+    music_name = db.Column(db.String(80))
+    music_type = db.Column(db.String(200))
+    image = db.Column(db.String(400))
+    artist_name = db.Column(db.String(3))
+    date_and_time = db.Column(db.String(200))
+    venue = db.Column(db.String(200))
+    event_status = db.Column(db.String(200))
+    description = db.Column(db.String(200))
+    # ... Create the Comments db.relationship
+	# relation to call destination.comments and comment.destination
+    comments = db.relationship('Comment', backref='destination')
+
+    
+	
+    def __repr__(self): #string print method
+        return "<Name: {}>".format(self.name)
+
+class Comment(db.Model):
+    __tablename__ = 'comments'
+    id = db.Column(db.Integer, primary_key=True)
+    text = db.Column(db.String(400))
+    created_at = db.Column(db.DateTime, default=datetime.now())
+    #add the foreign keys
+    user_id = db.Column(db.Integer, db.ForeignKey('users.id'))
+    destination_id = db.Column(db.Integer, db.ForeignKey('destinations.id'))
+
+
+    def __repr__(self):
+        return "<Comment: {}>".format(self.text)
