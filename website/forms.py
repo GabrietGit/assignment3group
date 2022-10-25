23aee932c90b1ff84fec81340cb9efe4f08e038d
Gabriel Largo <largo2610@gmail.com>
2022-10-25 11:43:48 +1000

    initial commit

--- website/forms.py
+++ website/forms.py
@@ -1,8 +1,20 @@
 
 from flask_wtf import FlaskForm
-from wtforms.fields import TextAreaField,SubmitField, StringField, PasswordField
+from wtforms.fields import TextAreaField, SubmitField, StringField, PasswordField
 from wtforms.validators import InputRequired, Length, Email, EqualTo
+from flask_wtf.file import FileRequired, FileField, FileAllowed
 
+ALLOWED_FILE = {'PNG','JPG','png','jpg'}
+
+class EventsForm(FlaskForm):
+  name = StringField('Country', validators=[InputRequired()])
+  description = TextAreaField('Description', 
+            validators=[InputRequired()])
+  image = FileField('Destination Image', validators=[
+    FileRequired(message='Image cannot be empty'),
+    FileAllowed(ALLOWED_FILE, message='Only supports png,jpg,JPG,PNG')])
+  currency = StringField('Currency', validators=[InputRequired()])
+  submit = SubmitField("Create")
 
 #creates the login information
 class LoginForm(FlaskForm):
