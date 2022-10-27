ad0d142ce9d6e407e416a07afbdd823c77ada549
Gabriel Largo <largo2610@gmail.com>
2022-10-27 20:51:41 +1000

    Initial commit

--- website/forms.py
+++ website/forms.py
@@ -1,7 +1,7 @@
 
 from datetime import datetime
 from flask_wtf import FlaskForm
-from wtforms.fields import TextAreaField, SubmitField, StringField, PasswordField, SelectField
+from wtforms.fields import TextAreaField, SubmitField, StringField, PasswordField, SelectField, DateField
 from wtforms.validators import InputRequired, Length, Email, EqualTo
 from flask_wtf.file import FileRequired, FileField, FileAllowed
 
@@ -23,8 +23,8 @@ class EventsForm(FlaskForm):
   image = FileField('Upload Image', validators=[
     FileRequired(message='Image cannot be empty'),
     FileAllowed(ALLOWED_FILE, message='Only supports png,jpg,JPG,PNG')])
-  artist_name = StringField('Artist Name', validators=[InputRequired()])
-  date_and_time = datetime('Date and Time', validators=[InputRequired()])
+  artist_name = StringField('Artist Name', format='%Y-%m-%d'())
+  date_and_time = DateField('Date and Time', validators=[InputRequired()])
   venue = StringField('Venue', validators=[InputRequired()])
   event_status = StringField('Event Status', validators=[InputRequired()])
   enter_description = StringField('Enter a description for the event', validators=[InputRequired()])
