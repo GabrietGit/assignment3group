3d3ec0f56e19439a9301ee878cae59d17560bb3b
Eskie0 <flynot@yahoo.com>
2022-10-28 13:20:06 +1000

    added blueprint

--- website/events.py
+++ website/events.py
@@ -7,5 +7,7 @@ from werkzeug.utils import secure_filename
 #additional import:
 from flask_login import login_required, current_user
 
+bp = Blueprint('destination', __name__, url_prefix='/destinations')
 
-#def book(): g
+@bp.route
+def book():
