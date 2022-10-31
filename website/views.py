591457afcd7868e6f835bf785b32fc3dbaf37868
Gabriel Largo <largo2610@gmail.com>
2022-10-31 20:50:27 +1000

    commit

--- website/views.py
+++ website/views.py
@@ -1,15 +1,15 @@
 from flask import Blueprint, render_template, request, redirect,url_for
 from .models import Events
 
-bp = Blueprint('main', __name__)
+mainbp = Blueprint('main', __name__)
 
 
-@bp.route('/')
+@mainbp.route('/')
 def index():
     events = Events.query.all()    
     return render_template('index.html', events=events)
 
-@bp.route('/search')
+@mainbp.route('/search')
 def search():
     if request.args['search']:
         print(request.args['search'])
