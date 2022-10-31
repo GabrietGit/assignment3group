78f4f10c9f258bcce75ad7416f99459ba86712ee
Gabriel Largo <largo2610@gmail.com>
2022-10-31 11:01:59 +1000

    commit

--- website/events.py
+++ website/events.py
@@ -36,3 +36,17 @@ def create():
     #Always end with redirect when form is valid
     return redirect(url_for('destinations.event creation'))
   return render_template('destinations/event creations.html', form=form)
+
+def check_upload_file(form):
+  #get file data from form  
+  fp=form.image.data
+  filename=fp.filename
+  #get the current path of the module file… store image file relative to this path  
+  BASE_PATH=os.path.dirname(__file__)
+  #upload file location – directory of this file/static/image
+  upload_path=os.path.join(BASE_PATH,'static/image',secure_filename(filename))
+  #store relative path in DB as image location in HTML is relative
+  db_upload_path='/static/image/' + secure_filename(filename)
+  #save the file and return the db upload path  
+  fp.save(upload_path)
+  return db_upload_path
