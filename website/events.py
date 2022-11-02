748e052f0b3f95b51d6ee7e1b0e651bd2e2ca326
Gabriel Largo <largo2610@gmail.com>
2022-11-02 21:36:47 +1000

    commit

--- website/events.py
+++ website/events.py
@@ -60,7 +60,3 @@ def check_upload_file(form):
   #save the file and return the db upload path  
   fp.save(upload_path)
   return db_upload_path
-
-class CommentForm(FlaskForm):
-  text = TextAreaField('Comment', [InputRequired()])
-  submit = SubmitField('Create')
