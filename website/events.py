7eb80c0d04a06957bc05dffb8c7bb0bc9dec589a
Gabriel Largo <largo2610@gmail.com>
2022-11-03 17:42:32 +1000

    commit

--- website/events.py
+++ website/events.py
@@ -28,37 +28,4 @@ def book():
         print('Successfully booked event', 'success')
     return render_template('events/event details.html', form=form) 
 
-@bp.route('/create', methods = ['GET', 'POST'])
-@login_required
-def create():
-  print('Method type: ', request.method)
-  form = EventsForm()
-  if form.validate_on_submit():
-    #call the function that checks and returns image
-    db_file_path=check_upload_file(form)
-    events=Events(name=form.name.data,description=form.description.data, 
-    image=db_file_path,currency=form.currency.data)
-    # add the object to the db session
-    db.session.add(events)
-    # commit to the database
-    db.session.commit()
-    print('Successfully created new travel destination', 'success')
-    #Always end with redirect when form is valid
-    return redirect(url_for('events.create'))
-  return render_template('events/event_creation.html', form=form)
-
-def check_upload_file(form):
-  #get file data from form  
-  fp=form.image.data
-  filename=fp.filename
-  #get the current path of the module file… store image file relative to this path  
-  BASE_PATH=os.path.dirname(__file__)
-  #upload file location – directory of this file/static/image
-  upload_path=os.path.join(BASE_PATH,'static/image',secure_filename(filename))
-  #store relative path in DB as image location in HTML is relative
-  db_upload_path='/static/image/' + secure_filename(filename)
-  #save the file and return the db upload path  
-  fp.save(upload_path)
-  return db_upload_path
-
   
