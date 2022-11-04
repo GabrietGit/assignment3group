e968883740b43e6e57900728c841687faac969f9
JoanneH7 <n11283378@qut.edu.au>
2022-11-04 13:08:58 +1000

    changes

--- website/views.py
+++ website/views.py
@@ -28,6 +28,10 @@ def search():
 def event_creation():
     return render_template('destinations/event_creation.html')
 
+@mainbp.route('/event details')
+def event_details():
+    return render_template('destinations/event details.html')
+
 @mainbp.route('/user')
 def user():
     return render_template('destinations/user.html')
